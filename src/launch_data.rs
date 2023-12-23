cf39489064c10d08e298e5d2cc9f135ad6e42777
pedohorse <13556996+pedohorse@users.noreply.github.com>
2023-12-23 01:30:49 +0100

    control data refactor WIP

--- src/launch_data.rs
+++ src/launch_data.rs
@@ -1,38 +1,143 @@
-use crate::proc::{create_process, terminate_child};
+use crate::running_process_data::LaunchedProcess;
 use crate::InstallationsData;
-use std::io;
-use std::process::{Child, ExitStatus};
-use std::sync::{Arc, Mutex};
+use std::{sync::{Arc, Mutex, MutexGuard}, io, process::ExitStatus};
 
-pub struct LaunchedProcess {
-    running_process: Child,
-    original_installation: Arc<Mutex<InstallationsData>>,
+pub struct ControlButtonsData {
+    _process: Option<LaunchedProcess>,
+    _command_label: String,
+    _command: String,
+    _args: Vec<String>,
+    _current_installation: Option<Arc<Mutex<InstallationsData>>>,
+    _last_run_exit_code: Option<i32>,
+    _current_installation_changed_callback:
+        Option<Box<dyn FnMut(&ControlButtonsData, Option<&InstallationsData>) -> ()>>, // arg will be new and prev installations data
 }
 
-impl LaunchedProcess {
+impl ControlButtonsData {
     pub fn new(
-        install_data: &Arc<Mutex<InstallationsData>>,
-        program: &str,
-        args: &Vec<String>,
-    ) -> io::Result<LaunchedProcess> {
-        let install_data = install_data.clone();
-        let process = create_process(program, args, install_data.lock().unwrap().base_path())?;
-        Ok(LaunchedProcess {
-            running_process: process,
-            original_installation: install_data,
-        })
+        installations: Option<&Arc<Mutex<InstallationsData>>>,
+        command_label: &str,
+        command: &str,
+        args: Vec<&str>,
+    ) -> ControlButtonsData {
+        ControlButtonsData {
+            _process: None,
+            _command_label: command_label.to_owned(),
+            _command: command.to_owned(),
+            _args: args.into_iter().map(|x| x.to_owned()).collect(),
+            _current_installation: if let Some(x) = installations {
+                Some(x.clone())
+            } else {
+                None
+            },
+            _last_run_exit_code: None,
+            _current_installation_changed_callback: None,
+        }
     }
 
-    pub fn send_terminate_signal(&self) -> io::Result<()> {
-        terminate_child(&self.running_process)
+    pub fn install_location_changed(
+        &mut self,
+        install_data: Option<&Arc<Mutex<InstallationsData>>>,
+    ) {
+        let prev_installations_data = std::mem::replace(
+            &mut self._current_installation,
+            if let Some(installations) = install_data {
+                Some(installations.clone())
+            } else {
+                None
+            },
+        );
+
+        let mut callback_maybe =
+            std::mem::replace(&mut self._current_installation_changed_callback, None);
+        // soooo, callback won't be able to see itself on control data..?
+        //  a bit strange this way, but i guess we can go with it
+
+        if let Some(ref mut callback) = callback_maybe {
+            //let prev_data =
+            if let Some(ref x) = prev_installations_data {
+                callback(self, Some(&x.lock().expect("failed to lock")));
+            } else {
+                callback(self, None);
+            };
+        };
+
+        self._current_installation_changed_callback = callback_maybe;
     }
 
+    pub fn set_install_location_changed_callback(&mut self, callback: Option<Box<dyn FnMut(&ControlButtonsData, Option<&InstallationsData>) -> ()>>) {
+        self._current_installation_changed_callback = callback;
+    }
+
+    pub fn start_process(&mut self) -> io::Result<()> {
+        self._process = match self._current_installation {
+            Some(ref installations) => {
+                match LaunchedProcess::new(installations, &self._command, &self._args) {
+                    Ok(p) => {
+                        Some(p)
+                    },
+                    Err(e) => {
+                        return Err(e);
+                    }
+                }
+            }
+            None => {
+                return Err(io::Error::new(io::ErrorKind::Other, "installation data is not set"));
+            }
+        };
+        Ok(())
+    }
+
+    pub fn is_process_running(&self) -> bool {
+        if let Some(_) = self._process {
+            true
+        } else {
+            false
+        }
+    }
+    
+    /// TODO: add docstrings explaining behaviour
     pub fn try_wait(&mut self) -> io::Result<Option<ExitStatus>> {
-        self.running_process.try_wait()
+        match self._process {
+            Some(ref mut proc) => {
+                let status_maybe = proc.try_wait();
+                if let Ok(Some(exit_status)) = status_maybe {
+                    self._process = None;
+                    let exit_code = exit_status.code().unwrap_or(-1); // read code() help to see why we rewrap this option
+                    self._last_run_exit_code = Some(exit_code);
+                }
+                status_maybe
+            }
+            None => Ok(None)
+        }
     }
 
-    pub fn wait(&mut self) -> io::Result<ExitStatus> {
-        self.running_process.wait()
+    pub fn is_current_installation_set(&self) -> bool {
+        if let Some(_) = self._current_installation {
+            true
+        } else {
+            false
+        }
+    }
+
+    pub fn current_installation(&self) -> Option<MutexGuard<InstallationsData>> {
+        match self._current_installation {
+            Some(ref mutexed_installations) => {
+                Some(mutexed_installations.lock().unwrap())
+            }
+            None => None
+        }
+    }
+
+    pub fn process(&self) -> Option<&LaunchedProcess> {
+        self._process.as_ref()
+    }
+
+    pub fn command(&self) -> &str {
+        &self._command
+    }
+
+    pub fn command_label(&self) -> &str {
+        &self._command_label
     }
 }
-
--- src/launch_widget.rs
+++ src/launch_widget.rs
@@ -1,80 +1,12 @@
-use crate::launch_data::LaunchedProcess;
-use crate::proc::{create_process, terminate_child};
+use crate::launch_data::ControlButtonsData;
 use crate::widgets::{Widget, WidgetCallbacks};
 use crate::InstallationsData;
 use fltk::button::Button;
 use fltk::{app, frame::Frame, group::Flex, prelude::*};
 use std::cell::RefCell;
-use std::ffi::OsStr;
-use std::io;
-use std::path::{PathBuf, Component};
-use std::process::{Child, Command};
+use std::path::{Component, PathBuf};
 use std::rc::Rc;
 use std::sync::{Arc, Mutex};
-
-struct ControlButtonsData {
-    process: Option<LaunchedProcess>,
-    command_label: String,
-    command: String,
-    args: Vec<String>,
-    current_installation: Option<Arc<Mutex<InstallationsData>>>,
-    last_run_exit_code: Option<i32>,
-    current_installation_changed_callback:
-        Option<Box<dyn FnMut(&ControlButtonsData, Option<&InstallationsData>) -> ()>>, // arg will be new and prev installations data
-}
-
-impl ControlButtonsData {
-    pub fn new(
-        installations: Option<&Arc<Mutex<InstallationsData>>>,
-        command_label: &str,
-        command: &str,
-        args: Vec<&str>,
-    ) -> ControlButtonsData {
-        ControlButtonsData {
-            process: None,
-            command_label: command_label.to_owned(),
-            command: command.to_owned(),
-            args: args.into_iter().map(|x| x.to_owned()).collect(),
-            current_installation: if let Some(x) = installations {
-                Some(x.clone())
-            } else {
-                None
-            },
-            last_run_exit_code: None,
-            current_installation_changed_callback: None,
-        }
-    }
-
-    pub fn install_location_changed(
-        &mut self,
-        install_data: Option<&Arc<Mutex<InstallationsData>>>,
-    ) {
-        let prev_installations_data = std::mem::replace(
-            &mut self.current_installation,
-            if let Some(installations) = install_data {
-                Some(installations.clone())
-            } else {
-                None
-            },
-        );
-
-        let mut callback_maybe =
-            std::mem::replace(&mut self.current_installation_changed_callback, None);
-        // soooo, callback won't be able to see itself on control data..?
-        //  a bit strange this way, but i guess we can go with it
-
-        if let Some(ref mut callback) = callback_maybe {
-            //let prev_data =
-            if let Some(ref x) = prev_installations_data {
-                callback(self, Some(&x.lock().expect("failed to lock")));
-            } else {
-                callback(self, None);
-            };
-        };
-
-        self.current_installation_changed_callback = callback_maybe;
-    }
-}
 
 pub struct LaunchWidget {
     scheduler_launch_data: Rc<RefCell<ControlButtonsData>>,
@@ -88,9 +20,15 @@ impl WidgetCallbacks for LaunchWidget {
         _path: &PathBuf,
         install_data: Option<&Arc<Mutex<InstallationsData>>>,
     ) {
-        self.scheduler_launch_data.borrow_mut().install_location_changed(install_data);
-        self.worker_pool_launch_data.borrow_mut().install_location_changed(install_data);
-        self.viewer_launch_data.borrow_mut().install_location_changed(install_data);
+        self.scheduler_launch_data
+            .borrow_mut()
+            .install_location_changed(install_data);
+        self.worker_pool_launch_data
+            .borrow_mut()
+            .install_location_changed(install_data);
+        self.viewer_launch_data
+            .borrow_mut()
+            .install_location_changed(install_data);
     }
 }
 
@@ -153,7 +91,7 @@ impl LaunchWidget {
         button_box.end();
 
         let info_box = Flex::default_fill().column();
-        Frame::default().with_label(&control_data.borrow().command_label);
+        Frame::default().with_label(&control_data.borrow().command_label());
         let mut info_label1 = Flex::default_fill().row();
         info_label1.fixed(&Frame::default().with_label("base:"), 48);
         let info_label_running_root = Frame::default().with_label("not running");
@@ -164,7 +102,7 @@ impl LaunchWidget {
 
         // init state
         stop_button.deactivate();
-        if let None = control_data.borrow_mut().current_installation {
+        if !control_data.borrow_mut().is_current_installation_set() {
             start_button.deactivate();
         }
 
@@ -176,18 +114,14 @@ impl LaunchWidget {
         let mut info_label_running_root_cl = info_label_running_root.clone();
         app::add_timeout3(1.0, move |handle| {
             let mut data = control_data_ref.borrow_mut();
-            let proc = if let Some(ref mut x) = data.process {
-                x
-            } else {
+            if !data.is_process_running() {
                 app::repeat_timeout3(2.0, handle);
                 return;
             };
 
-            match proc.try_wait() {
+            match data.try_wait() {
                 Ok(Some(status)) => {
-                    data.process = None;
                     let exit_code = status.code().unwrap_or(-1); // read code() help to see why we rewrap this option
-                    data.last_run_exit_code = Some(exit_code);
 
                     match exit_code {
                         0 => status_label_cl.set_label("⚪ finished OK"),
@@ -216,40 +150,64 @@ impl LaunchWidget {
         let mut info_label_running_root_cl = info_label_running_root.clone();
         start_button.set_callback(move |_| {
             let mut data = control_data_ref.borrow_mut();
-            if let Some(_) = data.process {
+            if let Some(_) = data.process() {
                 eprintln!("start button: process already started!");
                 return;
             }
 
-            data.process = match data.current_installation {
-                Some(ref installations) => {
-                    match LaunchedProcess::new(installations, &data.command, &data.args) {
-                        Ok(p) => {
-                            info_label_running_root_cl.set_label(
-                                &installations.lock().unwrap().base_path()
-                                    .components()
-                                    .rev()
-                                    .take(2)
-                                    .collect::<Vec<Component>>()
-                                    .into_iter()
-                                    .rev()
-                                    .collect::<PathBuf>()
-                                    .to_string_lossy()
-                            );
-                            Some(p)
-                        },
-                        Err(e) => {
-                            eprintln!("failed to start process! {:?}", e);
-                            let err = format!("🔴 failed to start! {}", e.kind());
-                            status_label_cl.set_label(&err);
-                            return;
-                        }
-                    }
+            match data.start_process() {
+                Ok(()) => {
+                    info_label_running_root_cl.set_label(
+                        &data
+                            .current_installation()
+                            .expect("unexpected: installation data disappeared!")
+                            .base_path()
+                            .components()
+                            .rev()
+                            .take(2)
+                            .collect::<Vec<Component>>()
+                            .into_iter()
+                            .rev()
+                            .collect::<PathBuf>()
+                            .to_string_lossy(),
+                    );
                 }
-                None => {
+                Err(e) => {
+                    eprintln!("failed to start process! {:?}", e);
+                    let err = format!("🔴 failed to start {}: {}", data.command(), e.kind());
+                    status_label_cl.set_label(&err);
                     return;
                 }
             };
+            // data.process = match data.current_installation {
+            //     Some(ref installations) => {
+            //         match LaunchedProcess::new(installations, &data.command, &data.args) {
+            //             Ok(p) => {
+            //                 info_label_running_root_cl.set_label(
+            //                     &installations.lock().unwrap().base_path()
+            //                         .components()
+            //                         .rev()
+            //                         .take(2)
+            //                         .collect::<Vec<Component>>()
+            //                         .into_iter()
+            //                         .rev()
+            //                         .collect::<PathBuf>()
+            //                         .to_string_lossy()
+            //                 );
+            //                 Some(p)
+            //             },
+            //             Err(e) => {
+            //                 eprintln!("failed to start process! {:?}", e);
+            //                 let err = format!("🔴 failed to start {}: {}", data.command, e.kind());
+            //                 status_label_cl.set_label(&err);
+            //                 return;
+            //             }
+            //         }
+            //     }
+            //     None => {
+            //         return;
+            //     }
+            // };
             start_button_cl.deactivate();
             stop_button_cl.activate();
             status_label_cl.set_label("🟢 running");
@@ -260,7 +218,7 @@ impl LaunchWidget {
         let mut stop_button_cl = stop_button.clone();
         stop_button.set_callback(move |_| {
             let data = control_data_ref.borrow_mut();
-            if let Some(ref proc) = data.process {
+            if let Some(ref proc) = data.process() {
                 if let Err(e) = proc.send_terminate_signal() {
                     eprintln!("failed to call terminate on child process cuz of: {:?}", e);
                     return;
@@ -272,26 +230,27 @@ impl LaunchWidget {
 
         control_data
             .borrow_mut()
-            .current_installation_changed_callback =
-            Some(Box::new(move |data, old_install_data_maybe| {
-                if let Some(_) = data.process {
-                    // leave state change up to process finalizing
-                    return;
-                }
+            .set_install_location_changed_callback(Some(Box::new(
+                move |data, old_install_data_maybe| {
+                    if data.is_process_running() {
+                        // leave state change up to process finalizing
+                        return;
+                    }
 
-                if let Some(_) = old_install_data_maybe {
-                    if let None = data.current_installation {
-                        start_button.deactivate();
-                        stop_button.activate();
-                        status_label.set_label("⚪ invalid");
+                    if let Some(_) = old_install_data_maybe {
+                        if !data.is_current_installation_set() {
+                            start_button.deactivate();
+                            stop_button.activate();
+                            status_label.set_label("⚪ invalid");
+                        }
+                    } else {
+                        if data.is_current_installation_set() {
+                            start_button.activate();
+                            stop_button.deactivate();
+                            status_label.set_label("⚪ ready");
+                        }
                     }
-                } else {
-                    if let Some(_) = data.current_installation {
-                        start_button.activate();
-                        stop_button.deactivate();
-                        status_label.set_label("⚪ ready");
-                    }
-                }
-            }));
+                },
+            )));
     }
 }
