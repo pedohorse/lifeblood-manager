e6f0320187644689ccd1829023c92fbf91bb0122
pedohorse <13556996+pedohorse@users.noreply.github.com>
2024-01-16 17:32:49 +0100

    fix win run/break

--- src/proc/win.rs
+++ src/proc/win.rs
@@ -2,26 +2,27 @@ use std::{
     io,
     os::windows::process::CommandExt,
     path::Path,
-    process::{Command, Child},
+    process::{Command, Child, Stdio},
 };
 use winconsole::console::generate_ctrl_event;
 
-const DETACHED_PROCESS: u32 = 0x00000008;
+// const DETACHED_PROCESS: u32 = 0x00000008;
 const CREATE_NEW_PROCESS_GROUP: u32 = 0x00000200;  // This is needed for pid to be equal to group id
-const CREATE_NO_WINDOW: u32 = 0x08000000;
+// const CREATE_NO_WINDOW: u32 = 0x08000000;
 
 
 pub fn create_process(program: &str, args: &Vec<String>, cwd: &Path) -> io::Result<Child> {
     println!("starting {:?}", program);
     Command::new(cwd.join(program))
-        .creation_flags(DETACHED_PROCESS | CREATE_NEW_PROCESS_GROUP | CREATE_NO_WINDOW)
+        .creation_flags(CREATE_NEW_PROCESS_GROUP)
         .args(args)
+        .stdin(Stdio::null())
         .current_dir(cwd)
         .spawn()
 }
 
 pub fn terminate_child(child: &Child) -> io::Result<()> {
-    match generate_ctrl_event(false, child.id()) {
+    match generate_ctrl_event(true, child.id()) {
         Ok(_) => return Ok(()),
         Err(e) => {
             eprintln!("error terminating process: {:?}", e);
