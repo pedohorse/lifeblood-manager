3643022cf3e4c314eab3418de570cf80059e57bb
pedohorse <13556996+pedohorse@users.noreply.github.com>
2024-03-05 22:20:50 +0100

    add schema tests

--- tests/test_envres.rs
+++ tests/test_envres.rs
@@ -1,5 +1,5 @@
 use lifeblood_manager::config_data_collection::ConfigDataCollection;
-use lifeblood_manager::config_data::ConfigData;
+use lifeblood_manager::config_data::{ConfigData, ConfigError};
 use home;
 
 #[test]
@@ -11,34 +11,98 @@ fn test_default_location() {
 
 #[test]
 fn test_validation_syntax() {
+    //
+    // good ones
     assert!(if let Ok(()) = ConfigData::validate_config_text(
         r#"
-[packages.foo.1.2.3]
+[packages.foo."1.2.3"]
 qet="aj qooq lalala"
 env.FOOBAR.append = ["/q/w/e", "/zxc"]
 "#
     ) { true } else { false });
 
+    //
     // bad ones
-    assert!(if let Err(_) = ConfigData::validate_config_text(
+    assert!(if let Err(ConfigError::SyntaxError(_, _)) = ConfigData::validate_config_text(
         r#"
-[packages.foo.1.2.3]
+[packages.foo."1.2.3"]
 qet="aj qooq lalala'
 env.FOOBAR.append = ["/q/w/e", "/zxc"]
 "#
     ) { true } else { false });
-    assert!(if let Err(_) = ConfigData::validate_config_text(
+    assert!(if let Err(ConfigError::SyntaxError(_, _)) = ConfigData::validate_config_text(
         r#"
-[packages.foo.1.2.3]
+[packages.foo."1.2.3"]
 qet="aj qooq lalala"
 env.FOO BAR.append = ["/q/w/e", "/zxc"]
 "#
     ) { true } else { false });
-    assert!(if let Err(_) = ConfigData::validate_config_text(
+    assert!(if let Err(ConfigError::SyntaxError(_, _)) = ConfigData::validate_config_text(
+        r#"
+[packages.foo."1.2.3"]
+qet="aj qooq lalala"
+env.FOOBAR.append = ["/q/w/e" "/zxc"]
+"#
+    ) { true } else { false });
+}
+
+#[test]
+fn test_validation_schema1() {
+    //
+    // good ones
+    assert!(if let Ok(()) = ConfigData::validate_config_text(
+        r#"
+[packages.foo."1.2.3"]
+qet="aj qooq lalala"
+env.FOOBAR.append = ["/q/w/e", "/zxc"]
+"#
+    ) { true } else { false });
+
+    //
+    // bad ones
+    assert!(if let Err(ConfigError::SchemaError(_)) = ConfigData::validate_config_text(
+        r#"
+packages=1.2
+"#
+    ) { true } else { false });
+    assert!(if let Err(ConfigError::SchemaError(_)) = ConfigData::validate_config_text(
+        r#"
+packages.foo = 2
+"#
+    ) { true } else { false });
+    assert!(if let Err(ConfigError::SchemaError(_)) = ConfigData::validate_config_text(
         r#"
 [packages.foo.1.2.3]
 qet="aj qooq lalala"
-env.FOOBAR.append = ["/q/w/e" "/zxc"]
+env.FOOBAR.append = ["/q/w/e", "/zxc"]
+"#
+    ) { true } else { false });
+    assert!(if let Err(ConfigError::SchemaError(_)) = ConfigData::validate_config_text(
+        r#"
+[packages.foo."1.2"]
+qet="aj qooq lalala"
+env.FOOBAR.append = ["/q/w/e", "/zxc"]
+"#
+    ) { true } else { false });
+    assert!(if let Err(ConfigError::SchemaError(_)) = ConfigData::validate_config_text(
+        r#"
+[packages.foo."1.2.3.4"]
+qet="aj qooq lalala"
+env.FOOBAR.append = ["/q/w/e", "/zxc"]
+"#
+    ) { true } else { false });
+    assert!(if let Err(ConfigError::SchemaError(_)) = ConfigData::validate_config_text(
+        r#"
+[packages.foo."1.2.q"]
+qet="aj qooq lalala"
+env.FOOBAR.append = ["/q/w/e", "/zxc"]
+"#
+    ) { true } else { false });
+    assert!(if let Err(ConfigError::SchemaError(_)) = ConfigData::validate_config_text(
+        r#"
+[packages.foo."1.2..3"]
+qet="aj qooq lalala"
+env.FOOBAR.append = ["/q/w/e", "/zxc"]
 "#
     ) { true } else { false });
 }
