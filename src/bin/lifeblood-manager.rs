97bb86795e27ce58678455fa80928ddac571cbff
pedohorse <13556996+pedohorse@users.noreply.github.com>
2024-10-12 11:45:13 +0200

    tray-item integration test

--- src/bin/lifeblood-manager.rs
+++ src/bin/lifeblood-manager.rs
@@ -1,19 +1,32 @@
+use fltk::button::{CheckButton, ToggleButton};
 use fltk::enums::CallbackTrigger;
+use fltk::window::DoubleWindow;
 use fltk::{
     app, button::Button, dialog::NativeFileChooser, frame::Frame, group::Flex, group::Tabs,
     input::FileInput, prelude::*, window::Window,
 };
 use lifeblood_manager::{
-    theme::*, InstallationWidget, InstallationsData, LaunchWidget, Widget, WidgetCallbacks, StandardEnvResolverConfigWidget, BUILD_INFO
+    theme::*, InstallationWidget, InstallationsData, LaunchWidget, StandardEnvResolverConfigWidget,
+    Widget, WidgetCallbacks, BUILD_INFO,
 };
 use std::env::current_dir;
 use std::path::PathBuf;
-use std::sync::{Arc, Mutex};
+use std::sync::mpsc::TryRecvError;
+use std::sync::{mpsc, Arc, Mutex};
+use tray_item::{IconSource, TrayItem};
 
 pub struct MainWidget {
     base_path_input: FileInput,
     sub_widgets: Vec<Arc<Mutex<dyn WidgetCallbacks>>>,
     install_data: Option<Arc<Mutex<InstallationsData>>>,
+    tray_item: Option<TrayItem>,
+    tray_command_sender: mpsc::Sender<TrayMessage>,
+}
+
+#[derive(Debug)]
+enum TrayMessage {
+    Quit,
+    Show,
 }
 
 impl MainWidget {
@@ -22,7 +35,7 @@ impl MainWidget {
         // let mut build_info = Flex::default().row();
         // Frame::default();
         // let build_label = Frame::default().with_label(BUILD_INFO);
-        
+
         // // 25% margin below is a pure guess
         // build_info.fixed(&build_label, (fltk::draw::measure(BUILD_INFO, true).0 as f32 * 1.25) as i32);
         // build_info.end();
@@ -39,11 +52,14 @@ impl MainWidget {
         (browse_button, base_input)
     }
 
-    pub fn new(path: &PathBuf) -> Arc<Mutex<Self>> {
+    pub fn new(path: &PathBuf, wind: &mut DoubleWindow, do_tray: bool) -> Arc<Mutex<Self>> {
         let mut flex = Flex::default_fill().column();
         // one shared install location
         // base path input
         let (mut browse_button, base_input) = Self::init_base_path_input(&mut flex);
+
+        let mut tray_checkbox = CheckButton::default().with_label("stay in tray");
+        tray_checkbox.set(do_tray);
 
         let path_warning_label = Frame::default().with_label("");
         flex.fixed(&path_warning_label, ITEM_HEIGHT);
@@ -69,13 +85,54 @@ impl MainWidget {
 
         flex.end();
 
+        let (tx, rx) = mpsc::channel();
+        // widget data
         let widget = Arc::new(Mutex::new(MainWidget {
             base_path_input: base_input,
             sub_widgets: widgets,
             install_data: None,
+            tray_item: None,
+            tray_command_sender: tx,
         }));
+        
+        app::add_timeout3(0.5, {
+            let mut wind = wind.clone();
+            let widget = widget.clone();
+            move |handle| {
+                match rx.try_recv() {
+                    Ok(message) => {
+                        println!("i have received {:?}", message);
+                        match message {
+                            TrayMessage::Show => {
+                                wind.show();
+                            },
+                            TrayMessage::Quit => {
+                                widget.lock().unwrap().remove_tray_item();
+                                wind.hide();
+                            }
+                        }
+                        app::repeat_timeout3(0.5, handle);
+                    }
+                    Err(TryRecvError::Empty) => {
+                        app::repeat_timeout3(0.5, handle);
+                    }
+                    Err(TryRecvError::Disconnected) => (), // close control channel
+                };
+            }
+        });
 
         // callbacks
+
+        tray_checkbox.set_callback({
+            let widget = widget.clone();
+            move |chb| {
+                if chb.is_checked() {
+                    widget.lock().unwrap().add_tray_item();
+                } else {
+                    widget.lock().unwrap().remove_tray_item();
+                }
+            }
+        });
 
         // tab changed
         tabs.set_trigger(CallbackTrigger::Changed); // according to docs, default is Released
@@ -91,7 +148,7 @@ impl MainWidget {
                     return;
                 };
                 let tab_index = w.find(&selected_wgt) as usize;
-                
+
                 let sub_widgets = &widget.lock().unwrap().sub_widgets;
                 sub_widgets[tab_index].lock().unwrap().on_tab_selected();
             }
@@ -127,6 +184,63 @@ impl MainWidget {
         widget.lock().unwrap().change_install_dir(path);
 
         widget
+    }
+
+    pub fn has_tray_item(&self) -> bool {
+        if let Some(_) = self.tray_item {
+            true
+        } else {
+            false
+        }
+    }
+
+    pub fn remove_tray_item(&mut self) {
+        if !self.has_tray_item() {
+            return;
+        }
+        if let Some(tray) = self.tray_item.take() {
+            drop(tray);
+        }
+    }
+
+    pub fn add_tray_item(&mut self) {
+        if self.has_tray_item() {
+            return;
+        }
+        // initialize tray item
+        let icon = IconSource::Resource("()");
+        let mut tray_maybe = if let Ok(x) = TrayItem::new("foofoo", icon) {
+            Some(x)
+        } else {
+            None
+        };
+
+        if let Some(ref mut tray) = tray_maybe {
+            tray.add_menu_item("Show", {
+                let tx = self.tray_command_sender.clone();
+                move || {
+                    tx.send(TrayMessage::Show).unwrap_or_else(|_| {
+                        println!("failed to communicate from tray item");
+                    });
+                }
+            })
+            .unwrap();
+            tray.add_menu_item("Quit", {
+                let tx = self.tray_command_sender.clone();
+                move || {
+                    tx.send(TrayMessage::Quit).unwrap_or_else(|_| {
+                        println!("failed to communicate from tray item");
+                    });
+                }
+            })
+            .unwrap();
+
+            
+        } else {
+            eprintln!("failed to initialize tray item");
+        }
+
+        self.tray_item = tray_maybe;
     }
 
     pub fn change_install_dir(&mut self, new_path: &PathBuf) {
@@ -166,13 +280,25 @@ fn main() {
         .with_size(650, 600)
         .with_label(&format!("Lifeblood Manager {}", BUILD_INFO));
 
-    MainWidget::new(&current_dir);
+    let main_widget = MainWidget::new(&current_dir, &mut wind, false);
+    // TODO: i've made all this widget-centric instead of data-centric, and now have trouble separating
+    // widget from tray events.
+    // Ideally this needs to be refactored so that control data is passed TO widgets, not created by them
+    // then multiple interfaces (tray interaction, widgets, etc) can interact with the same data
 
     wind.end();
 
     wind.make_resizable(true);
     wind.show();
-    app.run().unwrap();
+
+    loop {
+        // "event" loop
+        app::wait_for(0.0).expect("event loop broke");
+        if !wind.shown() && !main_widget.lock().unwrap().has_tray_item() {
+            break;
+        }
+    }
+    //app.run().unwrap();
     app::delete_widget(wind); // deleting widgets delets lambdas holding arcs to self
 
     // // Theming
