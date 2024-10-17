aadb61ede0f0c1f7f67f66db14356348b23aacba
pedohorse <13556996+pedohorse@users.noreply.github.com>
2024-10-17 22:56:31 +0200

    add generate_tray_items interface method

--- src/widgets.rs
+++ src/widgets.rs
@@ -2,11 +2,13 @@ use std::sync::Mutex;
 use std::sync::Arc;
 use std::path::PathBuf;
 use crate::installation_data::InstallationsData;
+use crate::tray_manager::TrayManager;
 use fltk::group::Flex;
 
 pub trait WidgetCallbacks {
     fn install_location_changed(&mut self, path: &PathBuf, install_data: Option<&Arc<Mutex<InstallationsData>>>);
     fn on_tab_selected(&mut self);
+    fn generate_tray_items(&mut self, tray_manager: &mut TrayManager);
 }
 
 pub trait Widget : WidgetCallbacks {
