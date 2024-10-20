1d948be8c5cdc2813e1726e6510183f8f000bc8d
pedohorse <13556996+pedohorse@users.noreply.github.com>
2024-10-20 13:38:08 +0200

    allow ephemeral configs not backed by files

--- src/main_widget_config.rs
+++ src/main_widget_config.rs
@@ -18,14 +18,14 @@ enum ExtraFieldValue {
 
 #[derive(serde::Deserialize, serde::Serialize)]
 struct ConfigData {
-    base_install_dir: PathBuf,  // use this as main dir
+    base_install_dir: PathBuf, // use this as main dir
     launch_ids_to_autostart: HashSet<String>,
     extra_fields: HashMap<String, ExtraFieldValue>,
     // additional launch data parameters may be stored here, or even the whole launch data?
 }
 
 pub struct MainWidgetConfig {
-    config_path: PathBuf,
+    config_path: Option<PathBuf>,
     config_data: ConfigData,
 }
 
@@ -33,10 +33,22 @@ impl MainWidgetConfig {
     ///
     /// create new config, IGNORING anything already present in the given base dir
     pub fn new(base_dir: &Path) -> MainWidgetConfig {
+        Self::new_full(Some(base_dir), base_dir)
+    }
+
+    pub fn new_ephemeral(base_dir: &Path) -> MainWidgetConfig {
+        Self::new_full(None, base_dir)
+    }
+
+    fn new_full(config_dir: Option<&Path>, installation_dir: &Path) -> MainWidgetConfig {
         MainWidgetConfig {
-            config_path: base_dir.join("lifeblood-manager.config"),
+            config_path: if let Some(dir) = config_dir {
+                Some(dir.join("lifeblood-manager.config"))
+            } else {
+                None
+            },
             config_data: ConfigData {
-                base_install_dir: base_dir.to_path_buf(),
+                base_install_dir: installation_dir.to_path_buf(),
                 launch_ids_to_autostart: HashSet::new(),
                 extra_fields: HashMap::new(),
             },
@@ -92,27 +104,35 @@ impl MainWidgetConfig {
     }
 
     pub fn write_to_file(&self) -> Result<(), Error> {
-        let config_string = match toml::to_string_pretty(&self.config_data) {
-            Ok(s) => s,
-            Err(e) => {
-                return Err(Error::new(ErrorKind::InvalidData, e));
-            }
-        };
-        println!("[DEBUG] writing config to: {:?}", &self.config_path);
-        fs::write(&self.config_path, config_string)?;
+        if let Some(ref config_file_path) = self.config_path {
+            let config_string = match toml::to_string_pretty(&self.config_data) {
+                Ok(s) => s,
+                Err(e) => {
+                    return Err(Error::new(ErrorKind::InvalidData, e));
+                }
+            };
+            println!("[DEBUG] writing config to: {:?}", config_file_path);
+            fs::write(config_file_path, config_string)?;
+        } else {
+            println!("[DEBUG] not saving ephemeral config");
+        }
         Ok(())
     }
 
     pub fn reload_from_file(&mut self) -> Result<(), Error> {
-        println!("[DEBUG] reading config from: {:?}", &self.config_path);
-        let config_text = fs::read_to_string(&self.config_path)?;
-        let config_data = match toml::from_str(&config_text) {
-            Ok(conf) => conf,
-            Err(e) => {
-                return Err(Error::new(ErrorKind::InvalidData, e.message()));
-            }
-        };
-        self.config_data = config_data;
+        if let Some(ref config_file_path) = self.config_path {
+            println!("[DEBUG] reading config from: {:?}", config_file_path);
+            let config_text = fs::read_to_string(config_file_path)?;
+            let config_data = match toml::from_str(&config_text) {
+                Ok(conf) => conf,
+                Err(e) => {
+                    return Err(Error::new(ErrorKind::InvalidData, e.message()));
+                }
+            };
+            self.config_data = config_data;
+        } else {
+            println!("[DEBUG] not reloading ephemeral config");
+        }
         Ok(())
     }
 }
